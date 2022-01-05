28addfdf9027a64c5fcc75d134e4f0ec8feb3a17
Moin <76849575+Moinuddin9777@users.noreply.github.com>
2022-01-06 02:14:52 +0530

    Update dash_main.py

--- avocado_prices/dash_main.py
+++ avocado_prices/dash_main.py
@@ -5,7 +5,7 @@ import pandas as pd
 import plotly.express as px
 from dash.dependencies import Input, Output
 
-path = "C:/Users/irfan/OneDrive/Desktop/MO TECH/avocado-updated-2020.csv"
+path = "C:/Users/irfan/OneDrive/Desktop/MO TECH/avocado-updated-2020.csv" #change path
 avo = pd.read_csv(path)
 
 app = dash.Dash()
